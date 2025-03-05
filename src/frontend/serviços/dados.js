b3a7acf103d9d96a1485ee5172d53855fbd18252
Julia Carolina <julia.sousa8@fatec.sp.gov.br>
2025-03-05 14:04:40 -0300

    Versão Backup

--- src/frontend/serviços/dados.js
+++ src/frontend/serviços/dados.js
@@ -2,10 +2,10 @@ import axios from "axios";
 
 const enderecoAPI = axios.create({ baseURL: "http://localhost:5000/endereco" });
 
-async function insereEndereco(id, endereco) {
+async function criarEndereco(id, endereco) {
     await enderecoAPI.post(`/${id}`, endereco);
 }
 
 export {
-    insereEndereco
-}
+    criarEndereco
+} 
