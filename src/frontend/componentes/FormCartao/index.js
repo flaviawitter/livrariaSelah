3087fcfb73f1f531d58c33c469dd2b632b09f1c8
flaviawitter <flaviawitter@gmail.com>
2025-03-10 13:39:17 -0300

    Alterações Front FormCartao

--- src/frontend/componentes/FormCartao/index.js
+++ src/frontend/componentes/FormCartao/index.js
@@ -1,4 +1,5 @@
 import Input from '../Input'
+import Select from "../Select"
 import styled from 'styled-components'
 
 const FormContainer = styled.section`
@@ -15,7 +16,6 @@ const Titulo = styled.h2`
     width: 100%;
     margin-bottom: 5px;
 `
-
 const Opcao = styled.li`
     font-size: 16px;
     display: flex;
@@ -29,7 +29,6 @@ const Opcao = styled.li`
     min-width: 80px;
     color: #C7511B;
 `
-
 const Opcoes = styled.ul`
     display: flex;
     flex-wrap: wrap;
@@ -38,11 +37,25 @@ const Opcoes = styled.ul`
     list-style-type: none;
     margin-top: 0;
 `
+const CheckboxLabel = styled.label`
+    color: #095F54;
+    font-size: 16px;
+    margin-left: 8px;
+    display: flex;
+    align-items: center;
+    font-family: "Bookochi";
+    letter-spacing: 0.22em;
+`
+const bandeiras = ["MasterCard", "Visa", "Elo", "Outro"]
+
 function FormCartao({ register }) {
     return (
         <FormContainer>
             <Titulo style={{ fontFamily: "Bookochi", letterSpacing: "0.22em" }}>Cartões Cadastrados</Titulo>
             <Opcoes>
+            <li key={"apelidoCartao"} style={{ width: "48%" }}>
+                    <Input  placeholder={"Apelido do Cartão"} {...register("apelidoCartao")} />
+            </li>
             <li key={"numeroCartao"} style={{ width: "48%" }}>
                     <Input  placeholder={"Número do Cartão"} {...register("numeroCartao")} />
             </li>
@@ -54,6 +67,13 @@ function FormCartao({ register }) {
             </li>
             <li key={"nomeTitular"} style={{ width: "48%" }}>
                     <Input placeholder={"Nome do Titular"} {...register("nomeTitular")} />
+            </li>
+            <li key={"bandeiraCartao"} style={{ width: "48%" }}>
+            <Select options={bandeiras} placeholder="Selecione a bandeira" registro={"bandeiraCartao"} register={register} />
+            </li>
+            <li key={"cartaoPreferencial"} style={{ width: "48%", display: "flex", alignItems: "center" }}>
+                    <input type="checkbox" {...register("cartaoPreferencial")} />
+                    <CheckboxLabel>Cartão Preferencial</CheckboxLabel>
             </li>
 
             </Opcoes>
